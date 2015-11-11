75fbda4e368e6cea26ec1f5d8846ec5f6d978d93
Joel Nieman <niemanjoel@gmail.com>
2015-11-11 12:31:13 -0500

    added view controller for camera

---
+++ StateChamps/CameraViewController.swift
@@ -0,0 +1,55 @@
+//
+//  CameraViewController.swift
+//  StateChamps
+//
+//  Created by Joel Nieman on 11/11/15.
+//  Copyright © 2015 JoelNieman. All rights reserved.
+//
+
+import UIKit
+
+class CameraViewController: UIViewController, UIImagePickerControllerDelegate {
+
+    @IBOutlet weak var userImageView: UIImageView!
+    
+    let imagePicker = UIImagePickerController()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        //imagepicker for camera and photo library access
+//        imagePicker.delegate = self
+    }
+
+
+    @IBAction func captureImageButton(sender: AnyObject) {
+        imagePicker.allowsEditing = true
+        imagePicker.sourceType = .Camera
+        
+        presentViewController(imagePicker, animated: true, completion: nil)
+    }
+
+    func imagePickerController(picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : AnyObject]) {
+        if let pickedImage = info[UIImagePickerControllerOriginalImage] as? UIImage {
+            userImageView!.contentMode = .ScaleAspectFit
+            userImageView!.image = pickedImage
+        }
+        
+        dismissViewControllerAnimated(true) {
+        }
+    }
+    
+}
+
+//func takeANewPhoto(sender: UIButton) {
+//    if UIImagePickerController.availableCaptureModesForCameraDevice(.Rear) != nil {
+//        imagePicker.allowsEditing = false
+//        imagePicker.sourceType = UIImagePickerControllerSourceType.Camera
+//        imagePicker.cameraCaptureMode = .Photo
+//    }
+//        
+//    else if UIImagePickerController.availableCaptureModesForCameraDevice(.Front) != nil {
+//        imagePicker.allowsEditing = false
+//        imagePicker.sourceType = UIImagePickerControllerSourceType.Camera
+//        imagePicker.cameraCaptureMode = .Photo
+//}
