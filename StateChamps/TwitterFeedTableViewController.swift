552a96d9eebcac5bb334f4bd7dbe03ba64f94c2f
Joel Nieman <niemanjoel@gmail.com>
2015-12-15 22:21:30 -0500

    changed news view controller to a tableViewController displaying State Champs Tweets

--- StateChamps/TwitterFeedTableViewController.swift
+++ StateChamps/TwitterFeedTableViewController.swift
@@ -1,21 +1,19 @@
 //
-//  TwitterViewController.swift
+//  TwitterFeedTableViewController.swift
 //  StateChamps
 //
-//  Created by Joel Nieman on 11/11/15.
+//  Created by Joel Nieman on 12/15/15.
 //  Copyright © 2015 JoelNieman. All rights reserved.
 //
 
 import UIKit
 import TwitterKit
 
-class TwitterViewController: TWTRTimelineViewController {
+class TwitterFeedTableViewController: TWTRTimelineViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
         let client = TWTRAPIClient()
         self.dataSource = TWTRUserTimelineDataSource(screenName: "statechampsnet", APIClient: client)
-        
-        tableView.contentInset = UIEdgeInsetsMake(20.0, 0.0, 0.0, 0.0)
     }
 }
